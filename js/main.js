f8af0068d6d17be485e8a72606530595c0b5f7bc
Hong-min1010 <qza7367@gmail.com>
2025-05-08 23:34:05 +0900

    header hover 적용 완료

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(".menu button").click(function() {
-    console.log("click");
-});
+// HTML문서가 모두 로드 된 후 내부 코드 실행
+$(document).ready(function () {
+    // .menu_main 클래스를 가진 요소 위에 마우스를 올리면 로직 실행
+    $(".menu_main").hover(function(){
+        // 마우스를 올리면 로직 실행(1) .mini_title slideDown
+      $(this).find(".mini_title").stop().slideDown();
+    },
+    // 마우스를 빼면 로직 실행(2) 
+    function(){
+        // 반대로 모두 slideUp
+      $(this).find(".mini_title").stop().slideUp();
+    });
+  });
